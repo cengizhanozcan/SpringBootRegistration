7765b46a869d5c3d69d28f12a9d9806ce2bcc558
Cengizhan Ozcan <cengizhan_ozcan@hotmail.com>
2017-04-25 23:28:40 +0300

    First commit

---
+++ src/main/java/tr/com/ceng/RegistrationApplication.java
@@ -0,0 +1,12 @@
+package tr.com.ceng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationApplication.class, args);
+	}
+}
